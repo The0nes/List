0d877fdd8aea813ccdd3bff51cf56baf51dab370
The0nes <93207468+The0nes@users.noreply.github.com>
2023-03-15 09:50:33 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+colors = ["Red", "Blue", "Black", "Purple", "Green"]
+print(colors[0])
+print(colors[2])
+print(colors[4])
+colors.append("Teal")
+print(colors)
+
+paychecks = [55.41, 177.77, 124.67, 240.18]
+
+for i in range(len(paychecks)):
+    paychecks[i] +=
